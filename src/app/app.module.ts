2cde1c2ee3dfc5b1770f639e772f9aad351a7fe9
Abdelrahman kamal <abdelrahmannkamal@gmail.com>
2024-07-31 13:49:00 +0300

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
-import { NgModule } from '@angular/core';
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+      BrowserAnimationsModule,
+
+      ServiceWorkerModule.register('ngsw-worker.js', {
+        enabled: !isDevMode(),
+        // Register the ServiceWorker as soon as the application is stable
+        // or after 30 seconds (whichever comes first).
+        registrationStrategy: 'registerWhenStable:30000'
+      })
+],
   providers: [
-    provideClientHydration()
-  ],
+    provideClientHydration(),
+      
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
